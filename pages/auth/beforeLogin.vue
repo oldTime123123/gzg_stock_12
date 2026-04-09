5dc2fed606ae18b8fab0dd83b18630f1906e0ca7
xiong <xiongjie19991111@gmail.com>
2026-04-09 17:06:12 +0800

    a

--- pages/auth/beforeLogin.vue
+++ pages/auth/beforeLogin.vue
@@ -13,7 +13,7 @@ onMounted(() => {
   <div class="beforeLoginPage flex   flex-col-reverse">
     <div class="px-3">
       <div class="pb-[15vh]">
-        <div class="contentBtn" @click="changePage('/auth/login')">{{ $t('login.l1') }}</div>
+        <div class="contentBtn" @click="changePage('/auth/login')">{{ $t('login.l1') }}111111</div>
         <div class="mt-3 colorfff">
           {{ $t('login.l2') }} <span class="text-[#FFDA1C]  cursor-pointer" @click="changePage('/auth/register')">{{
             $t('login.l3') }}</span>
